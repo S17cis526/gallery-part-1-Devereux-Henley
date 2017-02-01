9dfb29e7e51a80a2f20f4f395ea679c391cf1327
Devereux Henley <devereux@ksu.edu>
2017-01-31 23:41:41 -0600

    galleria

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * server.js
@@ -11,26 +11,54 @@ const port = 3449;
 
 const fs = require('fs');
 
+const css = fs.readFileSync('gallery.css');
+const imageNames = [
+	'ace',
+	'chess',
+	'fern',
+	'bubble',
+	'mobile']
 const server = http.createServer((req, res) => {
 	switch(req.url) {
-		case "/chess":
+		case '/gallery':
+			var gHtml = imageNames.map((fname) => {
+				return '<img src="' + fname + '" alt="oops">'
+			}).join(' ');
+			var html = '<!doctype html>'
+				html += '<head>'
+				html += '<title>Dynamic Page</title>'
+				html += '<link href="gallery.css" rel="stylesheet" type="text/css">'
+				html += '</head>'
+				html += '<body>'
+				html += '<h1>Gallery</h1>'
+				html += gHtml
+				html += '<h1>Hello.</h1> Time is ' + Date.now()
+				html += '</body>'
+			res.setHeader('Content-Type', 'text/html');
+			res.end(html);
+			break;
+		case '/gallery.css':
+			res.setHeader('Content-Type', 'text/css');
+			res.end(css);
+			break;
+		case '/chess':
 			goodResponse(res, 'chess.jpg');
 			break;
-		case "/ace":
+		case '/ace':
 			goodResponse(res, 'ace.jpg');
 			break;
-		case "/bubble":
+		case '/bubble':
 			goodResponse(res, 'bubble.jpg');
 			break;
-		case "/fern":
+		case '/fern':
 			goodResponse(res, 'fern.jpg');
 			break;
-		case "/mobile":
+		case '/mobile':
 			goodResponse(res, 'mobile.jpg');
 			break;
 		default:
 			res.statusCode = 404;
-			res.statusMessage = "Not found";
+			res.statusMessage = 'Not found';
 			res.end();
 	}		
 });
@@ -40,18 +68,18 @@ function goodResponse(res, fname) {
 		if(err) {
 			console.error(err);
 			res.statusCode = 500;
-			res.statusMessage = "oops";
-			res.end("Ya dun messed up Aaron.");
+			res.statusMessage = 'oops';
+			res.end('Ya dun messed up Aaron.');
 			return;
 		}
 		else{
 			res.statusCode = 200;
-			res.setHeader("Content-Type", "image/jpeg");
+			res.setHeader('Content-Type', 'image/jpeg');
 			res.end(body);
 		}
 	});	
 }
 
 server.listen(port, () => {
-	console.log("Listening on Port " + port);
+	console.log('Listening on Port ' + port);
 });
