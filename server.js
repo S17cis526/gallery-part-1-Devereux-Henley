bbeb1439f928146d5fed27d3f5e7bc8ea8de74c3
Devereux Henley <devereux@ksu.edu>
2017-02-02 14:14:19 -0600

    serve from image folder

--- server.js
+++ server.js
@@ -12,62 +12,65 @@ const port = 3449;
 const fs = require('fs');
 
 const css = fs.readFileSync('gallery.css');
-const imageNames = [
-	'ace',
-	'chess',
-	'fern',
-	'bubble',
-	'mobile']
+
+function getImageNames(callback) {
+	fs.readdir('images/', function(err, fileNames) {
+		if(err) callback(err, undefined);
+		else callback(false, fileNames);
+	});
+}
+
+function imageNamesToTags(fileNames) {
+	return fileNames.map((fileName) => {
+		return `<img src="${fileName}" alt="${fileName}">`;
+	});
+}
+
+function buildGallery(imageNames) {
+	var gHtml = imageNamesToTags(imageNames);
+	var html = '<!doctype html>'
+	html += '<head>'
+	html += '<title>Dynamic Page</title>'
+	html += '<link href="gallery.css" rel="stylesheet" type="text/css">'
+	html += '</head>'
+	html += '<body>'
+	html += '<h1>Gallery</h1>'
+	html += gHtml
+	html += '<h1>Hello.</h1> Time is ' + Date.now()
+	html += '</body>'
+	return html;
+}
+
 const server = http.createServer((req, res) => {
 	switch(req.url) {
+		case '/':
 		case '/gallery':
-			var gHtml = imageNames.map((fname) => {
-				return '<img src="' + fname + '" alt="oops">'
-			}).join(' ');
-			var html = '<!doctype html>'
-				html += '<head>'
-				html += '<title>Dynamic Page</title>'
-				html += '<link href="gallery.css" rel="stylesheet" type="text/css">'
-				html += '</head>'
-				html += '<body>'
-				html += '<h1>Gallery</h1>'
-				html += gHtml
-				html += '<h1>Hello.</h1> Time is ' + Date.now()
-				html += '</body>'
-			res.setHeader('Content-Type', 'text/html');
-			res.end(html);
+			getImageNames((err, imageNames) => {
+				if(err) {
+					console.error(err);
+					res.statusCode = 500;
+					res.statusMessage = 'server error';
+					res.end();
+					return;
+				}
+				res.setHeader('Content-Type', 'text/html');
+				res.end(buildGallery(imageNames));
+			});	
 			break;
 		case '/gallery.css':
 			res.setHeader('Content-Type', 'text/css');
 			res.end(css);
-			break;
-		case '/chess':
-			goodResponse(res, 'chess.jpg');
-			break;
-		case '/ace':
-			goodResponse(res, 'ace.jpg');
-			break;
-		case '/bubble':
-			goodResponse(res, 'bubble.jpg');
-			break;
-		case '/fern':
-			goodResponse(res, 'fern.jpg');
-			break;
-		case '/mobile':
-			goodResponse(res, 'mobile.jpg');
-			break;
+			break;	
 		default:
-			res.statusCode = 404;
-			res.statusMessage = 'Not found';
-			res.end();
+			serveImage(res, req.url);
 	}		
 });
 
-function goodResponse(res, fname) {
-	fs.readFile('images/' + fname, (err, body) => {
+function serveImage(res, fname) {
+	fs.readFile('images' + fname, (err, body) => {
 		if(err) {
 			console.error(err);
-			res.statusCode = 500;
+			res.statusCode = 404;
 			res.statusMessage = 'oops';
 			res.end('Ya dun messed up Aaron.');
 			return;
